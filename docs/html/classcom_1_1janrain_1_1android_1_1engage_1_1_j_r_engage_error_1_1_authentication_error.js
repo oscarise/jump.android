4196337464c5352ce40ee53fb83408211820b7c9
Nathan <nathan@janrain.com>
2013-07-16 09:52:07 -0700

    republish docs from v4.0.12-5-g76fe276

--- docs/html/classcom_1_1janrain_1_1android_1_1engage_1_1_j_r_engage_error_1_1_authentication_error.js
+++
@@ -1,6 +0,0 @@
-var classcom_1_1janrain_1_1android_1_1engage_1_1_j_r_engage_error_1_1_authentication_error =
-[
-    [ "AUTHENTICATION_FAILED", "classcom_1_1janrain_1_1android_1_1engage_1_1_j_r_engage_error_1_1_authentication_error.html#a2a7bcbe1d3c039bfdd57b21077249ec7", null ],
-    [ "AUTHENTICATION_TOKEN_URL_FAILED", "classcom_1_1janrain_1_1android_1_1engage_1_1_j_r_engage_error_1_1_authentication_error.html#a18c2296e5a22102d9071c365020e693a", null ],
-    [ "AUTHENTICATION_CANCELED", "classcom_1_1janrain_1_1android_1_1engage_1_1_j_r_engage_error_1_1_authentication_error.html#a98b475356a69311f634e8dbcd4c2b092", null ]
-];
